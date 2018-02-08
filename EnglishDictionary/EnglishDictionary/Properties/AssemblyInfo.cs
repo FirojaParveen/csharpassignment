8c5db54598c268ac34e3c0519e506acfb253faa6
Firoja Parveen <firoja@promact.com>
2018-02-08 12:38:57 +0530

    commit

---
+++ EnglishDictionary/EnglishDictionary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EnglishDictionary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EnglishDictionary")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59a0fdb3-de43-47a2-a4a2-315a5dd374af")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Primeministe Dictionary/PrimeministerDictionary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.PrimeministerDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<Int16, string> PrimeministerList = new Dictionary<Int16, string>();
+
+            PrimeministerList.Add(1998, "Atal Bihari Vajpayee");
+            PrimeministerList.Add(2014, "Narendra Modi");
+            PrimeministerList.Add(2004, "Manmohan Singh");
+            //<i>Find the Prime minister of 2004
+            var myKey = PrimeministerList.FirstOrDefault(x => x.Key == 2004).Value;
+
+            Console.WriteLine("The Prime Minister of 2004: " + myKey);
+            //<ii>Add current prime minister in the dictionary
+            Console.WriteLine("After adding current Prime Minister in the dictionary");
+            PrimeministerList.Add(2018, "Narendra Modi");
+            foreach (KeyValuePair<Int16, string> pml in PrimeministerList)
+            {
+                Console.WriteLine("Name = {0}, Year = {1}", pml.Value, pml.Key);
+
+            }
+            //<iii>.Make a read - only dictionary.
+            //<iv>.Sort the dictionary by year
+            Console.WriteLine("The Sorted dictionary by year");
+
+            var sortedDict = from entry in PrimeministerList orderby entry.Key ascending select entry;
+            foreach (KeyValuePair<Int16, string> pml in sortedDict)
+            {
+                Console.WriteLine("Name = {0}, Year = {1}", pml.Value, pml.Key);
+
+            }
+
+
+
+
+
+        }
+    }
+}
